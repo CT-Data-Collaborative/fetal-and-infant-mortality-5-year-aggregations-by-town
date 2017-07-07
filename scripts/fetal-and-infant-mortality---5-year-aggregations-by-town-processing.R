166804dd1a27c4b8e54304de88572097c07f711e
jennadaly <jenna.m.daly@gmail.com>
2017-07-07 18:31:13 -0400

    Initial commit.

---
+++ scripts/fetal-and-infant-mortality---5-year-aggregations-by-town-processing.R
@@ -0,0 +1,10 @@
+library(dplyr)
+library(datapkg)
+
+##################################################################
+#
+# Processing Script for Fetal and Infant Mortality - 5-Year Aggregations by Town
+# Created by Jenna Daly
+# On 07/07/2017
+#
+##################################################################
